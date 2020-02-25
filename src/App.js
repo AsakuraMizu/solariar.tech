8d67f95f504e576575d1499d58d74efd6448d8cb
water-lift <2677294549@qq.com>
2020-02-25 10:35:25 +0800

    改成了hashrouter(coding.net不支持browserrouter...)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import Header from './components/header';
 import Home from './page/home';
 import Projects from './page/projects';
